892352dfdd4d414fdd5d8db0961b7761e827aa66
Clayton Souza <clayton.rssouza@gmail.com>
2023-06-28 19:46:28 -0300

    feat: implement config environment

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,20 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    federation({
-      remotes: {
-        photolist: "http://localhost:5001/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom", "react-router-dom"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const PROD_URL_PHOTO_LIST = env.VITE_URL_REMOTE_PHOTO_LIST;
+  const DEV_URL_PHOTO_LIST = "http://localhost:5001/";
+
+  return {
+    plugins: [
+      federation({
+        remotes: {
+          photo_list: `${
+            mode === "development" ? DEV_URL_PHOTO_LIST : PROD_URL_PHOTO_LIST
+          }assets/remoteEntry.js`,
+        },
+        shared: ["react", "react-dom", "react-router-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
