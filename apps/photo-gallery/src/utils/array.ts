face1b12ead4431e3c710b848c908553ab2f8e23
Clayton Souza <clayton.rssouza@gmail.com>
2023-07-11 07:25:58 -0300

    fix: photo-view

--- apps/photo-gallery/src/utils/array.ts
+++ apps/photo-gallery/src/utils/array.ts
@@ -1,10 +1,8 @@
-import crypto from "crypto";
-
 export function createAndShuffleArray(size: number): number[] {
   const array = Array.from({ length: size }, (_, index) => index + 1);
 
   for (let currentIndex = array.length - 1; currentIndex > 0; currentIndex--) {
-    const randomIndex = crypto.randomInt(0, currentIndex + 1);
+    const randomIndex = getRandomIntInclusive(1, currentIndex);
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
@@ -12,4 +10,16 @@ export function createAndShuffleArray(size: number): number[] {
   }
 
   return array;
+}
+
+function getRandomIntInclusive(min: number, max: number): number {
+  const randomBuffer = new Uint32Array(1);
+
+  window.crypto.getRandomValues(randomBuffer);
+
+  let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(randomNumber * (max - min + 1)) + min;
 }
